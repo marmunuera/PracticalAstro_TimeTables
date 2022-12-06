0a81a8c07aef7ab5e8a974462a38be1e3206b9dd
marmunuera <56566088+marmunuera@users.noreply.github.com>
2022-12-06 22:12:03 +0100

    functions and example

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PracticalAstro_TimeTables
 This repository can be used to download and read the time conversion tables for Assignment 1 of Practical Astrodynamics
+
+
+## What can be done:
+
+
--- main.py
+++ main.py
@@ -40,19 +40,91 @@ def get_table_correction_GPS(): # this function saves the most recent leap secon
     open("leapSecondsTable.txt", "wb").write(response.content)
     return 0
 
-def main():
-    # definition of necessary constants:
-    command= sys.argv[1]
-
+def check_for_Tables():
     file_finals = os.path.exists("finals.daily.txt")
         if not file_finals:
-            print("finals.daily.txt does not exist, it has to be downloaded")
+            print("File finals.daily.txt does not exist, it has to be downloaded")
             get_table_correction_UT1()
+            print("File downloaded!")
 
     file_leapSec = os.path.exists("leapSecondsTable.txt")
         if not file_finals:
-            print("leapSecondsTable.txt does not exist, it has to be downloaded")
+            print("File leapSecondsTable.txt does not exist, it has to be downloaded")
             get_table_correction_GPS()
+            print("File downloaded !")
+
+    print("All necessary tables are present in your directory.")
+    return 0
+
+def get_DUT1(): # get lists with MDJ and DUT1=UT1-UTC from the tables (UTC_to_UT1)
+
+    # open text file in read mode
+    text_file = open("finals.daily.txt", "r")
+    # read whole file to a string
+    data = []
+    data = text_file.read().split('\n')
+    text_file.close()
+
+    if data[-1]=="":
+        del data[-1]
+
+    MJD=[0]*len(data)
+    DUT1 = [0]*len(data)
+
+    for i in range(len(data)):
+        MJD_table = float(data[i][7:14])
+        DUT1=float(data[i][58:67])
+
+    return MJD, DUT1
+
+
+def get_TAI_UTC(): # get lists with MDJ and errors
+
+    # open text file in read mode
+    text_file = open("leapSecondsTable.txt", "r")
+    # read whole file to a string
+    data = []
+    data = text_file.read().split('\n')
+
+    if data[-1]=="":
+        del data[-1]
+
+    text_file.close()
+
+    MJD=[0]*len(data)
+    TAI_UTC_1=[0]*len(data)
+    TAI_UTC_2=[0]*len(data)
+    TAI_UTC_3=[0]*len(data)
+
+    for i in range(len(data)):
+        MJD[i] = float(data[i][17:26])
+        TAI_UTC_1[i]= float(data[i][38:42])
+        TAI_UTC_2[i]= float(data[i][59:65])
+        TAI_UTC_3[i]= float(data[i][70:78])
+
+    return MJD, TAI_UTC_1, TAI_UTC_2,TAI_UTC_3
+
+
+def main():
+    # definition of necessary constants:
+
+    #First I check that I have the tables in my directory or I download them
+    check_for_Tables()
+
+    # Get MJD and DUT1 lists
+    MJD_1, DUT1 = get_DUT1()
+
+    # get lists with MDJ and errors
+    MJD_2, TAI_UTC_1, TAI_UTC_2,TAI_UTC_3 = get_TAI_UTC()
+
+    # EXAMPLE OF HOW TO USE THE DATA:
+    print("\nI want to know the last DUT1 and for what MJD it is:")
+    print("MDJ: " + str(MJD_1[-1]) + " ; DUT1: " + str(DUT1[-1]))
+
+    print("\nI want to know the last MDJ for which there is a correction in leap seconds:")
+    print("MDJ: " + str(MJD_2[-1]) )
+
+    print("Done.")
 
 
 
